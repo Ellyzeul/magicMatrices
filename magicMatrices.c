7d184d083d80f7d9a184382a3da5f2596c887279
Gabriel Augusto <gabrielaug.240@gmail.com>
2019-10-28 15:14:02 -0300

    feat: Implementado método para soulção de matrizes de ordem par divisiveis apenas por quatro

--- magicMatrices.c
+++ magicMatrices.c
@@ -26,12 +26,44 @@ void oddMagicMatrixConstructor(int *A, const int N){
     }
 }
 
+void matrixFill(int *A, const int N){
+    int i, lim = N*N;
+    for(i = 0; i < lim; i++)
+        *(A + i) = i + 1;
+    return;
+}
+
+void fourMultipleMagicMatrixConstructor(int *A, const int N){
+    int i, j, constant = N*N + 1, section = N/4;
+    matrixFill(A, N);
+
+    for (i = 0; i < section; i++){
+        for (j = 0; j < section; j++)
+            *(A + N*i + j) = constant - *(A + N*i + j);
+    }
+    for (i = 0; i < section; i++){
+        for (j = 3 * section; j < N; j++)
+            *(A + N*i + j) = constant - *(A + N*i + j);
+    }
+    for (i = 3 * section; i < N; i++){
+        for (j = 0; j < section; j++)
+            *(A + N*i + j) = constant - *(A + N*i + j);
+    }
+    for (i = 3 * section; i < N; i++){
+        for (j = 3 * section; j < N; j++)
+            *(A + N*i + j) = constant - *(A + N*i + j);
+    }
+    for (i = section; i < 3 * section; i++){
+        for ( j = section; j < 3 * section; j++)
+            *(A + N*i + j) = constant - *(A + N*i + j);
+    }return;
+}
 
 
 int main(void){
-    int A[3][3];
-    oddMagicMatrixConstructor(A, 3);
-    matrixPrint(A, 3);
+    int A[4][4];
+    fourMultipleMagicMatrixConstructor(A, 4);
+    matrixPrint(A, 4);
     system("Pause");
     return(0);
 }
